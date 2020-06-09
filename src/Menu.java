3316fc51489f6306d5ca9411f7f7b0a0c9ddf92d
Marcin <19smarcins98@gmail.com>
2020-06-09 11:48:02 +0200

    Working game! Fuck yeah :DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD

--- src/Menu.java
+++ src/Menu.java
@@ -14,11 +14,10 @@ public class Menu {
 
             System.out.println("\n---------- MENU ----------");
             System.out.println("1. Classic checkers");
-            System.out.println("2. First come, first served!");
-            System.out.println("3. First blood");
-            System.out.println("4. 50 / 50");
-            System.out.println("5. Credits");
-            System.out.println("6. \"KONIEC\"");
+            System.out.println("2. First blood");
+            System.out.println("3. 50 / 50");
+            System.out.println("4. Credits");
+            System.out.println("5. \"KONIEC\"");
 
             System.out.println("Choose game mode: ");
             choose = scan.next().charAt(0);
@@ -28,18 +27,15 @@ public class Menu {
                     classic_checkers();
                     break;
                 case '2':
-                    first_come_first_served();
-                    break;
-                case '3':
                     first_blood();
                     break;
-                case '4':
+                case '3':
                     fifty_fifty();
                     break;
-                case '5':
+                case '4':
                     credits();
                     break;
-                case '6':
+                case '5':
                     System.out.println("Jeszcze tu wrócę!");
                     exit(0);
                 default:
@@ -59,37 +55,6 @@ public class Menu {
         board.setup_board();
 
         while(board.get_number_of_blue_units() > 0 && board.get_number_of_red_units() > 0){
-            board.display_board();
-            board.move_piece();
-        }
-
-        board.display_board();
-    }
-
-    public void first_come_first_served(){
-        Board board = new Board();
-        board.setup_board();
-
-        boolean condition = false;
-
-        while(!condition || (board.get_number_of_blue_units() > 0 && board.get_number_of_red_units() > 0)){
-
-            for(int i = 0; i < board.get_blue_pieces_list().size(); i++){
-                if((board.get_blue_pieces_list().get(i)).is_king() == true) {
-                    System.out.println("Blue player wins!");
-                    condition = true;
-                    break;
-                }
-            }
-
-            for(int i = 0; i < board.get_red_pieces_list().size(); i++){
-                if((board.get_red_pieces_list().get(i)).is_king() == true) {
-                    System.out.println("Red player wins!");
-                    condition = true;
-                    break;
-                }
-            }
-
             board.display_board();
             board.move_piece();
         }
